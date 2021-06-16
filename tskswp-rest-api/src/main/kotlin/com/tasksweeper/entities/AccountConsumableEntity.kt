0154fa1e636162ca94d464a91594219b8cd902cd
SerBasti <sbstcosta@gmail.com>
2021-06-16 18:49:50 +0100

    fixing some entities

--- tskswp-rest-api/src/main/kotlin/com/tasksweeper/entities/AccountConsumableEntity.kt
+++ tskswp-rest-api/src/main/kotlin/com/tasksweeper/entities/AccountConsumableEntity.kt
@@ -4,9 +4,9 @@ import org.jetbrains.exposed.sql.Table
 
 object AccountConsumable : Table("tskswp.account_consumable") {
     val username = varchar("username", 256).references(Account.username)
-    val consumableName = varchar("consumable_name", 256).references(Consumable.name)
+    val consumableId = long("consumable_id").references(Consumable.id)
     val quantity = long("quantity")
-    override val primaryKey = PrimaryKey(username, consumableName, name = "account_consumable_pkey")
+    override val primaryKey = PrimaryKey(username, consumableId, name = "account_consumable_pkey")
 }
 
 data class AccountConsumableDTO(
