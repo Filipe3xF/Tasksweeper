cf35b40adaba1a6800f10d8782784fe24bf294b9
Filipe Ferreira <filipe3xf@gmail.com>
2021-07-16 17:52:36 +0100

    Change http request hander to use https instead

--- tskswp_client/lib/services/http_requests/http_request_handler.dart
+++ tskswp_client/lib/services/http_requests/http_request_handler.dart
@@ -1,30 +1,39 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 const webHost = 'tasksweeper-web-api.herokuapp.com';
 
 class HttpHandler {
-
   static Future<String> postRequest(Object? body, String path) async {
     return (await http.post(
-      Uri.http(webHost, path),
+      Uri.https(webHost, path),
       headers: {'content-type': 'application/json'},
       body: jsonEncode(body),
-    )).body;
+    ))
+        .body;
   }
 
-  static Future<String> getRequestWithQuery(String jwt, String path, var query) async {
+  static Future<String> getRequestWithQuery(
+      String jwt, String path, var query) async {
     return (await http.get(
-      Uri.http(webHost, path, query),
-      headers: {'content-type': 'application/json', 'Authorization': 'Bearer $jwt'},
-    )).body;
+      Uri.https(webHost, path, query),
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer $jwt'
+      },
+    ))
+        .body;
   }
 
   static Future<String> getRequest(String jwt, String path) async {
     return (await http.get(
-      Uri.http(webHost, path),
-      headers: {'content-type': 'application/json', 'Authorization': 'Bearer $jwt'},
-    )).body;
+      Uri.https(webHost, path),
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer $jwt'
+      },
+    ))
+        .body;
   }
-
 }
