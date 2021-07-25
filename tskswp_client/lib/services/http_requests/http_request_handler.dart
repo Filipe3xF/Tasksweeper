c17d121cf0bf21f6d49737efc1aaead198fc1fa6
SerBasti <sbstcosta@gmail.com>
2021-07-25 13:18:52 +0100

    format code

--- tskswp_client/lib/services/http_requests/http_request_handler.dart
+++ tskswp_client/lib/services/http_requests/http_request_handler.dart
@@ -14,19 +14,29 @@ class HttpHandler {
         .body;
   }
 
-  static Future<String> postRequestWithAuthentication( Object? body,  String jwt, String path) async {
+  static Future<String> postRequestWithAuthentication(
+      Object? body, String jwt, String path) async {
     return (await http.post(
       Uri.https(webHost, path),
-      headers: {'content-type': 'application/json', 'Authorization': 'Bearer $jwt'},
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer $jwt'
+      },
       body: jsonEncode(body),
-    )).body;
+    ))
+        .body;
   }
 
-  static Future<String> getRequestWithQuery(String jwt, String path, var query) async {
+  static Future<String> getRequestWithQuery(
+      String jwt, String path, var query) async {
     return (await http.get(
       Uri.https(webHost, path, query),
-      headers: {'content-type': 'application/json', 'Authorization': 'Bearer $jwt'},
-    )).body;
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer $jwt'
+      },
+    ))
+        .body;
   }
 
   static Future<String> getRequest(String jwt, String path) async {
